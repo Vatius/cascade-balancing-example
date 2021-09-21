c572a889c68a19e3acfb12346a21ee1c37f42f62
Vadim B <mail@vatius.ru>
2021-09-21 11:34:54 +0300

    fix test

--- cmd/server/main_test.go
+++ cmd/server/main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	model "github.com/Vatius/cascade-balancing-example"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -11,7 +12,7 @@ import (
 func TestHandlerPostPayload(t *testing.T) {
 	testServer := httptest.NewServer(http.HandlerFunc(handlerPostPayload))
 	defer testServer.Close()
-	testObject := []Payload{{1, 2, 3, 4, 5}}
+	testObject := []model.Payload{{1, 2, 3, 4, 5}}
 	testBody, _ := json.Marshal(testObject)
 	res, err := http.Post(testServer.URL, "application/json", bytes.NewBuffer(testBody))
 	if err != nil {
